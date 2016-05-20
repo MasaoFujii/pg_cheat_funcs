d92411fe767be6b8177c79e448ed46c0f64067fa
MasaoFujii <masao.fujii@gmail.com>
2016-05-20 21:34:52 +0900

    Add regression test for pg_text_to_hex() and pg_hex_to_text().

--- sql/pg_cheat_funcs.sql
+++ sql/pg_cheat_funcs.sql
@@ -10,4 +10,8 @@ SELECT pg_eucjp('xa4', 'xa2');
 
 SELECT pg_set_next_xid(next_xid) = next_xid FROM pg_xid_assignment();
 
+SELECT pg_text_to_hex('PostgreSQL');
+SELECT pg_hex_to_text('506f737467726553514c');
+SELECT pg_hex_to_text(upper('506f737467726553514c'));
+
 DROP EXTENSION pg_cheat_funcs;
