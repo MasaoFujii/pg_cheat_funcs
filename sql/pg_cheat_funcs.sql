bf8e1e3975e7b11a94b0c1eea013557d74e92413
MasaoFujii <masao.fujii@gmail.com>
2017-10-17 02:13:13 +0900

    Add regression test for pg_get_priority() and pg_set_priority() functions.

--- sql/pg_cheat_funcs.sql
+++ sql/pg_cheat_funcs.sql
@@ -6,6 +6,9 @@ SELECT pg_stat_print_memory_context();
 
 SELECT pg_signal_process(pg_postmaster_pid(), 'HUP');
 
+SELECT pg_set_priority(pg_backend_pid(), 1);
+SELECT pg_get_priority(pg_backend_pid());
+
 SELECT pg_eucjp('xa4', 'xa2');
 
 SELECT pg_set_next_xid(next_xid) = next_xid FROM pg_xid_assignment();
